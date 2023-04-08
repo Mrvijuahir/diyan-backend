a3eff0c07d699458efe66fc59b6cd1a50c2847e6
Vijay <vahir9487@gmail.com>
2023-04-08 17:00:38 +0530

    update and delete

--- controllers/role.controller.js
+++ controllers/role.controller.js
@@ -1,4 +1,5 @@
-const { Roles } = require("../models");
+const { Roles, Users } = require("../models");
+const { Op } = require("sequelize");
 
 exports.addRole = async (req, res, next) => {
 	try {
@@ -29,14 +30,98 @@ exports.addRole = async (req, res, next) => {
 
 exports.getRole = async (req, res, next) => {
 	try {
-		const { id } = req.body;
-		const where = {};
-		where.id = id;
-		let role = await Roles.findAll(where);
+		const {
+			query: { role_name, status },
+		} = req;
+
+		const where = {
+			[Op.or]: [
+				{
+					role_name: {
+						[Op.eq]: role_name,
+					},
+				},
+				{
+					status: {
+						[Op.eq]: status,
+					},
+				},
+			],
+		};
+		let role = await Roles.findAll({ where });
+
 		res.status(200).json({
 			status: true,
 			message: "Successful.",
 			data: role,
+		});
+	} catch (error) {
+		next(error);
+	}
+};
+
+exports.getRoleById = async (req, res, next) => {
+	try {
+		const {
+			params: { id },
+		} = req;
+		let role = await Roles.findOne({
+			where: { id },
+		});
+		if (role) {
+			role = role.get();
+			res.status(200).json({
+				status: true,
+				message: "Successfull.",
+				data: role,
+			});
+		} else {
+			res.status(404).json({
+				status: false,
+				message: "Record not available",
+			});
+		}
+	} catch (error) {
+		next(error);
+	}
+};
+
+exports.updateRole = async (req, res, next) => {
+	try {
+		const {
+			params: { id },
+		} = req;
+		await Roles.update(req.body, {
+			where: {
+				id: id,
+			},
+		});
+		const updateValue = await Roles.findOne({
+			where: {
+				id: id,
+			},
+		});
+		res.status(200).json({
+			status: true,
+			message: "Update successful.",
+			data: updateValue.get(),
+		});
+	} catch (error) {
+		next(error);
+	}
+};
+
+exports.deleteRole = async (req, res, next) => {
+	try {
+		const {
+			params: { id },
+		} = req;
+		await Roles.destroy({
+			where: { id },
+		});
+		res.status(200).json({
+			status: true,
+			message: "Delete successful.",
 		});
 	} catch (error) {
 		next(error);
